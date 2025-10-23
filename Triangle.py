bf0de35b7ed32da4122f68b8153ed504708208ea
Benedict Martinez <benmart1738@gmail.com>
2025-10-23 17:34:24 -0400

    Fix classifyTriangle defects to satisfy test suite

--- Triangle.py
+++ Triangle.py
@@ -5,15 +5,12 @@ Updated Jan 21, 2018
 
 The primary goal of this file is to demonstrate a simple python program to classify triangles
 
-@author: jrr
-@author: rk
+@author: Benedict Martinez
+Pledge: I pledge my honor that I have abided by the Stevens Honor System
 """
 
 def classifyTriangle(a,b,c):
     """
-    Your correct code goes here...  Fix the faulty logic below until the code passes all of 
-    you test cases. 
-    
     This function returns a string with the type of triangle from three integer values
     corresponding to the lengths of the three sides of the Triangle.
     
@@ -27,31 +24,29 @@ def classifyTriangle(a,b,c):
       BEWARE: there may be a bug or two in this code
     """
 
-    # require that the input values be >= 0 and <= 200
+    # 1. Type check first
+    if not (isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
+        return 'InvalidInput'
+
+    # 2. Range / Sign
     if a > 200 or b > 200 or c > 200:
         return 'InvalidInput'
-        
-    if a <= 0 or b <= b or c <= 0:
+    if a <= 0 or b <= 0 or c <= 0:
         return 'InvalidInput'
-    
-    # verify that all 3 inputs are integers  
-    # Python's "isinstance(object,type) returns True if the object is of the specified type
-    if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
-        return 'InvalidInput';
-      
-    # This information was not in the requirements spec but 
-    # is important for correctness
-    # the sum of any two sides must be strictly less than the third side
-    # of the specified shape is not a triangle
-    if (a >= (b - c)) or (b >= (a - c)) or (c >= (a + b)):
+
+    # 3. Triangle Inequality (Strict)
+    if a + b <= c or a + c <= b or b + c <= a:
         return 'NotATriangle'
-        
-    # now we know that we have a valid triangle 
-    if a == b and b == a:
+
+    # 4. Classification
+    if a == b == c:
         return 'Equilateral'
-    elif ((a * 2) + (b * 2)) == (c * 2):
+
+    x, y, z = sorted((a, b, c))
+    if x*x + y*y == z*z:
         return 'Right'
-    elif (a != b) and  (b != c) and (a != b):
-        return 'Scalene'
-    else:
-        return 'Isoceles'
+
+    if a == b or b == c or a == c:
+        return 'Isosceles'
+
+    return 'Scalene'
